bbce57cfcd43c641f4c46a42583e2eb939153533
Nahfi <nahfi01@gmail.com>
2022-08-06 23:02:06 +0600

    copy button work

--- app.js
+++ app.js
@@ -13,12 +13,26 @@ Vue.createApp({
                 this.rotateZ = 0,
                 this.rotateX = 0,
                 this.rotateY = 0
+        },
+        copy() {
+            const copyItem = `transform:${this.box.transform}`
+            const el = document.createElement('textarea')
+            el.appendChild(document.createTextNode(copyItem))
+            el.style.position = 'absolute'
+            el.style.left = '-99999px'
+            const sections = document.getElementById('copy')
+            document.getElementById('copy').appendChild(el)
+            el.select(); //select textarea contenrs
+            document.execCommand("copy");
+            el.remove()
+
+            console.log(el)
         }
     },
     computed: {
         box() {
             return {
-                transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotatey(${this.rotateY}deg) rotateX(${this.rotateZ}deg)`
+                transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateX(${this.rotateZ}deg)`
             }
         }
     }

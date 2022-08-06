4bc2e5af400cd2d19ae2c0bef9878b4f748979b8
Nahfi <nahfi01@gmail.com>
2022-08-06 22:32:20 +0600

    basic vue project

---
+++ app.js
@@ -0,0 +1,26 @@
+Vue.createApp({
+    data() {
+        return {
+            perspective: 100,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0
+        }
+    },
+    methods: {
+        reset() {
+            this.perspective = 100,
+                this.rotateZ = 0,
+                this.rotateX = 0,
+                this.rotateY = 0
+        }
+    },
+    computed: {
+        box() {
+            return {
+                transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotatey(${this.rotateY}deg) rotateX(${this.rotateZ}deg)`
+            }
+        }
+    }
+
+}).mount('#app')
